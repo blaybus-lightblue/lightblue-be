3bc2030a8df1c7cbf629b5ad8ce1a1f503aab348
haeun <haeun9634@naver.com>
2025-08-17 12:24:02 +0900

    chore: 스프링부트 초기 세팅

---
+++ src/main/java/com/example/lightblue/LightblueApplication.java
@@ -0,0 +1,13 @@
+package com.example.lightblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightblueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightblueApplication.class, args);
+	}
+
+}
